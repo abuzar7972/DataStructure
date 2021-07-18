c364648a2490502ec0d883d3c3ccb13bb440f1d4
abuzar7972 <66540790+abuzar7972@users.noreply.github.com>
2021-07-18 12:17:50 +0530

    Cash counter main class

---
+++ CashCounter_04.java
@@ -0,0 +1,9 @@
+package com.que_04;
+
+public class CashCounter_04 {
+    public static void main(String[] args) {
+        QueueUT queueUT = new QueueUT();
+        queueUT.depositOrCredi();
+    }
+}
+
