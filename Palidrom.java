ba0ac3f8403037e704872831a7f0a8fe08515270
abuzar7972 <abushaikh7972@gmail.com>
2021-07-18 12:08:35 +0530

    Palidrome

---
+++ Palidrom.java
@@ -0,0 +1,8 @@
+package com.q5;
+
+public class Palidrom {
+    public static void main(String[] args) {
+        PalidromeUT palidromeUT = new PalidromeUT();
+        palidromeUT.check();
+    }
+}
